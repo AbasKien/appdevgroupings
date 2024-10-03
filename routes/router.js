8dbaf3156cd9364139633074f2f1ab497dd244d2
vincent acha <vnino1228@gmail.com>
2024-10-03 22:30:04 +0800

    router

--- routes/router.js
+++ routes/router.js
@@ -1,27 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const indexController = require("../controller/indexController.js");
 const adminController = require("../controller/adminController.js");
 const upload = require('../config/multer'); // Adjust the path accordingly
-const userController = require('../controller/userController')
 
-//index
-router.get('/', indexController.index);
+
 
 
 
 
 //admin
 router.get('/admin', adminController.admin);
+router.get('/admin/manage-products', adminController.manageProducts);
+router.post('/admin/manage-products', upload.single('productImage'), adminController.addProduct);
+router.get('/admin/manage-users', adminController.manageUsers);
+router.post('/admin/manage-products/delete/:id', adminController.deleteProduct);
 
 //user
-router.get('/login', userController.users); // Render login view
-router.get('/register', userController.registration); // Render registration view
-
-
-// Handle registration and login
-router.post('/register', userController.registrationHandler); // Handle registration
-router.post('/login', userController.loginHandler); // Handle login
 
 
 
